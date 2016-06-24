a6b5648a1302a17a7225e75f41ffdc4170dc8f46
Wen Zhenglin <wenzhenglin@bjjdsy.com.cn>
2016-06-24 17:38:18 +0800

    Add consul register

---
+++ reg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# registering a service
+
+service="$( cat <<eof
+{
+  "Name": "fileserver2",
+  "Tags": [
+    "master"
+  ],
+  "Address":"192.168.100.94",
+  "Port": 8000,
+  "Check": {
+    "HTTP": "http://192.168.100.94:8000",
+    "Interval": "3s"
+  }
+}
+eof
+)"
+
+url="http://192.168.100.94:8500/v1/agent/service/register"
+curl -X PUT "$url" -d "$service"
+
+# end.
